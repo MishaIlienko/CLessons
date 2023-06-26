26c0b31ba055d70cb33e5184ddd811f000c856d9
Misha <mnwahr@gmail.com>
2023-06-26 21:01:07 +0200

    New about Funtions

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,19 +1,26 @@
-﻿int a1 = 1;
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+
+int a1 = 1;
 int a2 = 2;
-int a3 = 4;
-int b1 = 15;
-int b2 = 25;
-int b3 = 20;
+int a3 = 4367;
+int b1 = 152987;
+int b2 = 253792;
+int b3 = 203;
 int c1 = 22;
 int c2 = 28;
-int c3 = 31;
-int max = a1;
-if (a2 > max) max = a2;
-if (a3 > max) max = a3;
-if (b1 > max) max = b1;
-if (b2 > max) max = b2;
-if (b3 > max) max = b3;
-if (c1 > max) max = c1;
-if (c2 > max) max = c2;
-if (c3 > max) max =c3;
+int c3 = 315;
+
+//int max1 = Max(a1,a2,a3);
+//int max2 = Max(b1,b2,b3);
+//int max3 = Max(c1,c2,c3);
+//int max = Max(max1,max2,max3);
+int max = Max(Max(a1,a2,a3), Max(b1,b2,b3), Max(c1,c2,c3));
 Console.WriteLine (max);
