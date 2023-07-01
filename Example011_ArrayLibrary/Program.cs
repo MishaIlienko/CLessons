3013862587c713d6faf5d4df611aa70eea66ad65
Misha <mnwahr@gmail.com>
2023-07-01 22:31:49 +0200

    Finish 011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray (int[]collection)
+﻿void FillArray (int[]collection) //Метод, чтобы присвоить в массиве рандомные значенния
 {
     int length = collection.Length;
     int index = 0;
@@ -8,7 +8,7 @@
         index++;
     }
 }
-void PrintArray(int[]col)
+void PrintArray(int[]col) // Метод для вывода значений массива
 {
     int count = col.Length;
     int position = 0;
@@ -18,6 +18,28 @@ void PrintArray(int[]col)
         position++;
     }
 }
-int[] array = new int[10];
+int IndexOf(int[]collection, int find) // Метод для поиска нужного значения
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        break;
+        }
+        index++;    
+    }
+    return position;
+
+}
+int[] array = new int[10]; // Определяем в массиве определенное колличество элементов
 FillArray(array);
+array[4] = 4;
+array[7] = 4;
 PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);
