c4803f046824ad9df833e5575104e1feb5224c5f
Misha <mnwahr@gmail.com>
2023-07-01 21:20:40 +0200

    New 011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,15 +1,23 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray (int[]collection)
 {
     int length = collection.Length;
     int index = 0;
     while(index < length)
     {
-
         collection[index] = new Random().Next(1, 10);
         index++;
     }
-
-
 }
-
-int[]array = new int[10];
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
