7059b25d8e1ec2793136ba92384ac5051557f6fd
Misha <mnwahr@gmail.com>
2023-08-15 15:17:00 +0200

    New 12 about 4thMethods

---
+++ Example12_4Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿// Метод №1 - Void, который НЕ принимает аргументов и НЕ возвращает значения.
+
+using System.Data;
+
+void Method1()
+{
+    System.Console.WriteLine("Текст1....");
+}
+Method1();
+
+// Meтод №2 - Void, который пнригимает аргументы, и НЕ возвращает значения.
+
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2("Текст2....");
+System.Console.WriteLine();
+
+// В методе 2, также можно указывать несколько аргументов, 
+//и вызывать разниыми способами (именнованными аргументами, и просто по порядку).
+
+void Method21(string sms, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(sms);
+        i++;
+    }
+
+
+}
+Method21(sms: "Текст3", count: 3);
+
+// Метод 3-го типа НЕ принимает аргументов и возвращает значение
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+System.Console.WriteLine(year);
+
+// 4-й тип метода принимает аргументы и возвращает значения
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(text: "Текст4 ", count: 3);
+System.Console.WriteLine(res);
